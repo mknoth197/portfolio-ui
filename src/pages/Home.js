1d0200bdd3f790c5f14024d2f8079f29bfabc462
Mitchell Knoth <mknoth197@gmail.com>
2024-08-15 20:44:03 -0500

    refactor: file structure change

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container, Typography } from '@mui/material';
+
+function Home() {
+  return (
+    <Container>
+      <Typography variant="h2">Welcome to My Portfolio</Typography>
+      <Typography variant="body1">This is the home page.</Typography>
+    </Container>
+  );
+}
+
+export default Home;
